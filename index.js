6fb363c13adde278bdfc648c09e0b7b96a9ccd29
ShaneEverittM <shane.everitt.murphy@gmail.com>
2021-04-07 00:33:19 -0700

    Add wrapper with docs.

---
+++ index.js
@@ -0,0 +1,30 @@
+// Pull in native module that we have no information about
+const basis = require('./basis.node')
+
+
+// Create thin wrappers that give Node context and WebStorm autocomplete
+
+/**
+ * A test function that just returns a pre-defined message.
+ *
+ * @returns {string}
+ */
+function greeting() {
+    return basis['greeting']()
+}
+
+
+/**
+ * A test function that adds two numbers
+ *
+ * @param {number} x
+ * @param {number} y
+ *
+ * @returns {number}
+ */
+function add(x, y) {
+    return basis['add'](x, y)
+}
+
+
+module.exports = {greeting, add}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
 use neon::prelude::*;
+use neon::result::Throw;
 
 pub fn greeting(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("Hello from basis"))
 }
 
+fn parse_number_arg(cx: &mut FunctionContext, idx: i32) -> Result<f64, Throw> {
+    Ok(cx.argument::<JsNumber>(idx)?.value(cx))
+}
+
+pub fn add(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let x = parse_number_arg(&mut cx, 0)?;
+    let y = parse_number_arg(&mut cx, 1)?;
+    Ok(cx.number(x + y))
+}
+
 #[neon::main]
 fn main(mut m: ModuleContext) -> NeonResult<()> {
-    m.export_function("greeting", greeting)
+    m.export_function("greeting", greeting)?;
+    m.export_function("add", add)?;
+    Ok(())
 }
